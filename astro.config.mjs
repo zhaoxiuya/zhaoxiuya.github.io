19507c88f152b30c494b0b8d82bb1c7db426bbef
zhaoxiuya <zhaoxiuya13@gmail.com>
2026-02-07 23:13:15 +0900

    tmp

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import mdx from '@astrojs/mdx';
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [mdx()],
-  site: "https//zhaoxiuya.github.io";
-  base: "/"
+  site: "https://zhaoxiuya.github.io",
+  base: "/",
 });
